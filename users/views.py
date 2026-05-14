d9c9a9003784565c1e92fcba3fba11d20391d4ca
dvy19 <divyadixit.311204@gmail.com>
2026-05-14 19:55:05 +0530

    database connected

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken, UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
